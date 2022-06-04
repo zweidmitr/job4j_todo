0e04d6ea58fe3a8c45f932c53c0bc260e9a3ab7c
zweidmitr <zweidmitr@gmail.com>
2022-06-04 11:59:13 +0300

    3. Лямбды и шаблон wrapper.

--- src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
@@ -2,10 +2,12 @@ package ru.job4j.todo.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class ItemDBStore {
@@ -15,101 +17,95 @@ public class ItemDBStore {
         this.sf = sf;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        session.save(item);
-
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+        );
     }
 
     public boolean update(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        int index = session.createQuery("update Item i set "
-                        + "i.name = :name, "
-                        + "i.description = :description, "
-                        + "i.created = :created, "
-                        + "i.done = :done "
-                        + "where i.id = :fId")
-                .setParameter("name", item.getName())
-                .setParameter("description", item.getDescription())
-                .setParameter("created", item.getCreated())
-                .setParameter("fId", item.getId())
-                .setParameter("done", item.isDone())
-                .executeUpdate();
-
-        session.getTransaction().commit();
-        session.close();
-        return index != 0;
+        return this.tx(
+                session -> {
+                    int index = session.createQuery("update Item i set "
+                                    + "i.name = :name, "
+                                    + "i.description = :description, "
+                                    + "i.created = :created, "
+                                    + "i.done = :done "
+                                    + "where i.id = :fId")
+                            .setParameter("name", item.getName())
+                            .setParameter("description", item.getDescription())
+                            .setParameter("created", item.getCreated())
+                            .setParameter("fId", item.getId())
+                            .setParameter("done", item.isDone())
+                            .executeUpdate();
+                    return index != 0;
+                }
+        );
     }
 
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        int index = session.createQuery("delete from Item i where i.id = :fId")
-                .setParameter("fId", id)
-                .executeUpdate();
-
-        session.getTransaction().commit();
-        session.close();
-        return index != 0;
+        return this.tx(
+                session -> {
+                    int index = session.createQuery("delete from Item i where i.id = :fId")
+                            .setParameter("fId", id)
+                            .executeUpdate();
+                    return index != 0;
+                }
+        );
     }
 
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        Item result = (Item) session.createQuery("from Item i where i.id = :fId")
-                .setParameter("fId", id)
-                .getSingleResult();
-
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> {
+                    Item result = (Item) session.createQuery("from Item i where i.id = :fId")
+                            .setParameter("fId", id)
+                            .getSingleResult();
+                    return result;
+                }
+        );
     }
 
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        List result = session.createQuery("from Item").list();
-
-        session.getTransaction();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item ").list()
+        );
     }
 
     public List<Item> findByDone(boolean isDone) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        List result = session.createQuery("from Item i where i.done = :isDone")
-                .setParameter("isDone", isDone).list();
-
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> session.createQuery("from Item i where i.done = :isDone")
+                        .setParameter("isDone", isDone).list()
+        );
     }
 
     public boolean setDone(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-
-        int result = session.createQuery("update Item  i set "
-                        + "i.done = :done where i.id = :fId")
-                .setParameter("done", true)
-                .setParameter("fId", id)
-                .executeUpdate();
-
-        session.getTransaction().commit();
-        session.close();
-        return result != 0;
+        return this.tx(
+                session -> {
+                    int index = session.createQuery("update Item  i set "
+                                    + "i.done = :done where i.id = :fId")
+                            .setParameter("done", true)
+                            .setParameter("fId", id)
+                            .executeUpdate();
+                    return index != 0;
+                }
+        );
     }
-
 }
