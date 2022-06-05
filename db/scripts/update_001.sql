b19f87910ad553a62e2d2860350378a341752e8f
zweidmitr <zweidmitr@gmail.com>
2022-06-05 23:24:55 +0300

    0. ToOne - correct

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,3 +1,11 @@
+create table if not exists users
+(
+    id       serial primary key,
+    name     text,
+    email    varchar unique,
+    password text
+);
+
 create table if not exists items
 (
     id          serial primary key,
@@ -6,12 +14,4 @@ create table if not exists items
     created     TIMESTAMP,
     done        boolean,
     user_id     int not null references users (id)
-);
-
-create table if not exists users
-(
-    id       serial primary key,
-    name     text,
-    email    varchar unique,
-    password text
 );
